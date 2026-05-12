0bbb8e1fee033cb1cbedbc93e0ee89c9ecb5cad5
eric <eric.gan@lannlife.com>
2026-05-12 16:48:50 +0800

    feat(cdc): add InsertEvent, UpdateEvent, DeleteEvent

---
+++ src/main/java/io/github/lystrosaurus/atlasmountain/cdc/event/InsertEvent.java
@@ -0,0 +1,21 @@
+package io.github.lystrosaurus.atlasmountain.cdc.event;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class InsertEvent {
+  private final String database;
+  private final String table;
+  private final List<Serializable[]> rows;
+  private final Instant timestamp;
+
+  public InsertEvent(String database, String table, List<Serializable[]> rows) {
+    this.database = database;
+    this.table = table;
+    this.rows = rows;
+    this.timestamp = Instant.now();
+  }
+}
---
+++ src/main/java/io/github/lystrosaurus/atlasmountain/cdc/event/UpdateEvent.java
@@ -0,0 +1,23 @@
+package io.github.lystrosaurus.atlasmountain.cdc.event;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+
+@Getter
+public class UpdateEvent {
+  private final String database;
+  private final String table;
+  private final List<Map.Entry<Serializable[], Serializable[]>> rows;
+  private final Instant timestamp;
+
+  public UpdateEvent(String database, String table,
+      List<Map.Entry<Serializable[], Serializable[]>> rows) {
+    this.database = database;
+    this.table = table;
+    this.rows = rows;
+    this.timestamp = Instant.now();
+  }
+}
